c1aaaaa0a03b5bcf66454bd2d07d738f6565ae40
MahadevWaghmode <45910438+MahadevWaghmode@users.noreply.github.com>
2020-12-30 18:19:10 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,10 @@
+#!C:\Users\Mahadev\AppData\Local\Programs\Python\Python37-32\python.exe
+print("Content-type: text/html")
+print()
+
+import cgi
+
+#form = cgi.FieldStorage()
+#se =  form.getvalue('search')
+a="vaibhav"
+print("<center><h1>Form Data : "+a)
